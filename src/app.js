ae6a77399974a79401810fc6cec47acfff229ac5
allemkarthik <allemkarthik@gmail.com>
2026-04-07 23:46:53 -0700

    middlewares

--- src/app.js
+++ src/app.js
@@ -1,42 +1,23 @@
 // create a server
 
-const express=require("express")
+const express = require("express");
 
-const app=express()
+const app = express();
 
-// this will only handle get call
-app.get("/test",(req,res)=>{
-    res.send({firstname:"karthik",lastname:"allem"})
-})
-
-// patch
-app.patch("/patch",(req,res)=>{
-    res.send("patch is done")
-})
-
-// put  
-
-app.put("/put",(req,res)=>{
-    res.send("put method")
-})
-
-// delete
-app.delete("/delete",(req,res)=>{
-    res.send("delete method")
-})
-
-// post
-app.use("/post",(req,res)=>{
-    res.send("data saved successfully in database")
-})
-// it will match all the http methods api calls
-app.use("/hello",(req,res)=>{
-    res.send("hello from the hello")
-})
-
+app.use(
+  "/user",
+  (req, res,next) => {
+    console.log("1 st console")
+    next()
+    res.send("1st response");
+  },
+  (req,res) => {
+    console.log("2nd console")
+    res.send("2nd response")
+  },
+);
 
 // server listener
-app.listen(3000,()=>{
-    console.log("server is listening successfully on port 3000....");
-    
+app.listen(3000, () => {
+  console.log("server is listening successfully on port 3000....");
 });
