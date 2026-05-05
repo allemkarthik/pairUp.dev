4ca3a43fc1d5eb138d8fa3f8e17d64e3b16a695b
allemkarthik <allemkarthik@gmail.com>
2026-05-04 23:21:20 -0700

    password change API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const profileRouter = express.Router();
+const bcrypt=require("bcrypt")
 
 // user auth middleware
 const { userAuth } = require("../middleware/auth");
@@ -37,5 +38,31 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     res.status(400).send("ERROR: " + err.message);
   }
 });
+
+
+// create a forgot password api
+profileRouter.patch("/profile/passwordchange", userAuth, async (req,res)=>{
+  try{
+    const user=req.user
+    const {oldPassword, newPassword}=req.body
+
+    // compare old password
+    const isOldPasswordValid= await bcrypt.compare(oldPassword, user.password)
+
+    if(!isOldPasswordValid){
+      throw new Error("Old password is Invalid")
+    }
+
+    // encrypt newpassword
+    const hashPassword= await bcrypt.hash(newPassword,10)
+
+    // update password in DB
+    user.password=hashPassword
+    await user.save()
+    res.send("password changed sucessfully")
+  }catch(err){
+    res.status(400).send("ERROR: " + err.message)
+  }
+})
 
 module.exports = profileRouter;
