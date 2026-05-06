6a74508d2a80a42c826701a0de507d3cadb201a5
allemkarthik <allemkarthik@gmail.com>
2026-05-06 00:25:08 -0700

    corner cases for connection request api

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequestModel = require("../models/connectionRequest");
+const User=require("../models/user")
 
 const requestRouter = express.Router();
 
+// sending connection request api
 requestRouter.post(
   "/request/send/:status/:toUserId",
   userAuth,
@@ -14,6 +16,37 @@ requestRouter.post(
       const toUserId = req.params.toUserId;
       const status = req.params.status;
 
+      //only two allowed entries
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid status type: " + status });
+      }
+
+      // to user itself connection request
+
+      
+
+      //   check for user not available in db
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        return res.status(404).json({ message: "user not found!" });
+      }
+
+      //   if duplicate connection request
+      const existingConnectionRequest = await ConnectionRequestModel.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+
+      if (existingConnectionRequest) {
+        return res
+          .status(400)
+          .send({ message: "connection request is already sent" });
+      }
       const connectionRequest = new ConnectionRequestModel({
         fromUserId,
         toUserId,
